f9f7fb2e969341442967eeba06ffcb4180fbb554
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2022-02-02 12:54:59 +0100

    fixup! Update list-versions.sh, update version_date.tsv

--- utils/list-versions/list-versions.sh
+++ utils/list-versions/list-versions.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-git tag --list 'v*-lts' 'v*-stable' --format='%(tag)	%(taggerdate:format:%F)' | grep '^v' | sort -rV
+# refname:strip=2: default tag name when format is not set
+# creatordate is always defined for all tags
+git tag --list 'v*-lts' 'v*-stable' --format='%(refname:strip=2)	%(creatordate:format:%F)' | grep '^v' | sort -rV
